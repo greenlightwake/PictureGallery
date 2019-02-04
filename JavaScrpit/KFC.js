63db85a48276d8dfa269016922c170b4d5d30142
greenlightwake <46201242+greenlightwake@users.noreply.github.com>
2019-02-04 13:35:34 +0800

    标签切换

--- JavaScrpit/KFC.js
+++ JavaScrpit/KFC.js
@@ -3,5 +3,8 @@ function showpic(_tupian)
     var oldPic=document.getElementById("默认图片");
     var picLink=_tupian.getAttribute("href");
     oldPic.setAttribute("src",picLink);
+    var _oldDescription=document.getElementById("默认描述文本");
+    var _descrpitiontext=_tupian.getAttribute("title");
+    _oldDescription.firstChild.nodeValue=_descrpitiontext;
     return true;
 }
